c816b28f53f12617b34acef4583fd21a0da72254
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-22 22:04:52 +0530

    [Biplab] Add: Max float value

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,11 +4,12 @@ namespace Generics
 {
         public class Program
         {
+           
             public static void Main(string[] args)
             {
-                MaxIntNum maxNumber = new MaxIntNum();
-                int MaximumNumber = maxNumber.MaxInteger(10, 20, 30);
-                Console.WriteLine("Maximum Integer is " + MaximumNumber);
+            MaxFloatNum maxFloatNum = new MaxFloatNum();
+            double MaximumNumber = maxFloatNum.MaximumFloat(1.2, 2.2, 0.2);
+            Console.WriteLine("Maximum Float Number is " + MaximumNumber);
 
             }
         }
