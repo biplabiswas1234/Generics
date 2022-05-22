176f5843f68e3dba9b9c09df94dc13b2ee758dd8
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-22 22:21:18 +0530

    [Biplab] Add: using Generics Max value

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -2,22 +2,21 @@
 
 namespace Generics
 {
-        public class Program
+    public class Program
+    {
+
+        public static void Main(string[] args)
         {
-           
-            public static void Main(string[] args)
-            {
             int[] intArray = { 19, 25, 46, 35, 97 };
             MaxNumber<int> maxNumber = new MaxNumber<int>(intArray);
-            Console.WriteLine("The Maximum Integer is " + maxNumber.MaxMethod());
+            maxNumber.PrintMax();
 
             double[] doubleArray = { 65.325, 11.336, 452.12, 56.48, 23.25 };
             MaxNumber<double> maxDouble = new MaxNumber<double>(doubleArray);
-            Console.WriteLine("The Maximum Float Value is " + maxDouble.MaxMethod());
-
+            maxDouble.PrintMax();
             string[] stringArray = { "Apple", "Grapes", "Banana", "Jackfruit" };
             MaxNumber<string> maxString = new MaxNumber<string>(stringArray);
-            Console.WriteLine("The Maximum String Value is " + maxString.MaxMethod());
+            maxString.PrintMax();
         }
-        }
+    }
 }
