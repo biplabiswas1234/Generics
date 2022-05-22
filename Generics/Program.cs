3078d81bf5b2d48870d639533ba1d0819861fee0
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-22 21:52:51 +0530

    [Biplab] Add: Max integer value

---
+++ Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Generics
+{
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                MaxIntNum maxNumber = new MaxIntNum();
+                int MaximumNumber = maxNumber.MaxInteger(10, 20, 30);
+                Console.WriteLine("Maximum Integer is " + MaximumNumber);
+
+            }
+        }
+}
